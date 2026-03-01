2479c8b39f79dba5801d6a5c9dd75f504340c854
InsoniacX <riandaarsyad@gmail.com>
2026-03-01 22:47:31 +0700

    Finished the Todo Project

---
+++ client/src/App.tsx
@@ -0,0 +1,22 @@
+/* eslint-disable react-refresh/only-export-components */
+import { Container, Stack } from "@chakra-ui/react";
+import Navbar from "./components/Navbar";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
+
+export const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api";
+
+export default function App() {
+  return (
+    <>
+      <Stack h="100vh">
+        <Navbar />
+        <Container>
+          <TodoForm />
+          <TodoList />
+        </Container>
+      </Stack>
+    </>
+  );
+}
